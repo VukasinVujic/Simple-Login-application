f157bd43c068487ac14581bf762f3a31aa3a9467
VukasinVujic <vukasinvujic021@gmail.com>
2019-07-01 17:03:13 +0200

    Model,migration,factory

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,15 +3,15 @@
 use Illuminate\Database\Seeder;
 use App\User;
 
-class DatabaseSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(UsersTableSeeder::class);
+        factory(User::class,10)->create();        
     }
 }
